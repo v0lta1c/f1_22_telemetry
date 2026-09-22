a1b35010d2b933c26faf4eb9d301ad23a4e97b1e
Mann Bilimoria <mann.bilimoria@gmail.com>
2026-09-22 20:34:48 +0300

    Update constants.h

--- src/constants.h
+++ src/constants.h
@@ -9,9 +9,6 @@
 const std::string Webhook_URL = "84.249.17.176";
 const int Webhook_PORT = 20003;
 
-const std::string Webhook_ProjektServer = "https://discord.com/api/webhooks/1266629985620197426/DvWC9zKP3O0k7T1KxE2AEpBC95wgZnoUcxc6PZ7cypny7HPu0VNHUo2skZHYiKaEJ9jY";
-const std::string Webhook_Interstellar = "https://discord.com/api/webhooks/1263968867290452009/SUpbGVLM1Rnb7wJwfxbOsuxW3wE_i4fdgWhlvCCSzKW6ZPrzsqzmAnEO-nz8_ZPFI7Ic";
-
 // IP and Port for the API server
 const int API_PORT = 8000;
 
